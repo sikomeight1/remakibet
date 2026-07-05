0831cc381b6cf1787dbc9bcbb3828a52619a5233
sikomeight1 <sikomeight1@gmail.com>
2026-07-05 16:21:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+document.querySelectorAll('nav a').forEach(link=>{
 
+link.addEventListener('click',function(e){
+
+e.preventDefault();
+
+const target=document.querySelector(this.getAttribute('href'));
+
+target.scrollIntoView({
+
+behavior:'smooth'
+
+});
+
+});
+
+});
+
+const cards=document.querySelectorAll('.card');
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity="1";
+
+entry.target.style.transform="translateY(0)";
+
+}
+
+});
+
+});
+
+cards.forEach(card=>{
+
+card.style.opacity="0";
+
+card.style.transform="translateY(40px)";
+
+card.style.transition="1s";
+
+observer.observe(card);
+
+});
